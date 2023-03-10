6245fc2d135f5b6156d136e8f9cb0cf36d5fb32f
The Dogebird <108379190+TheDogeBird@users.noreply.github.com>
2023-03-10 17:27:43 -0600

    Delete DogePAWS-RealBuild directory

--- DogePAWS-RealBuild/AppRelated.py
+++
@@ -1,3 +0,0 @@
-from main import app
-
-# Define app-related objects here, e.g. database connections, API clients, etc.
--- DogePAWS-RealBuild/InventoryMgmt.py
+++
@@ -1,3 +0,0 @@
-inventory = []
-
-# Define inventory management functionality here, e.g. database queries, data validation, etc.
